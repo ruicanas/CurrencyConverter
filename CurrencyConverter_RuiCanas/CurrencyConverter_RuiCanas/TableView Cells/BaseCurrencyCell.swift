1f0b7ec41e7cc97e8477fdf638bbd53550a809ce
Rui <ruijfcanas@gmail.com>
2021-04-06 10:11:18 +0100

    [DONE] Base Currency Cell

---
+++ CurrencyConverter_RuiCanas/CurrencyConverter_RuiCanas/TableView Cells/BaseCurrencyCell.swift
@@ -0,0 +1,23 @@
+//
+//  BaseCurrencyCell.swift
+//  CurrencyConverter_RuiCanas
+//
+//  Created by itsector on 06/04/2021.
+//
+
+import UIKit
+
+class BaseCurrencyCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
